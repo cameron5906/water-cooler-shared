d9cbe64b7621c605204030a57564c8659d6548b7
Cameron Cooper <cameron@cameron.pw>
2020-05-04 18:44:47 -0400

    Add initial typings

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,39 @@
-declare module "Payloads" {
-    export interface CreateAccountFromInvitationPayload {
-        invitationCode: string;
-        name: string;
-        email: string;
-        password: string;
-    }
-
-    export interface CreateOrganizationPayload {
-        name: string;
-        domain: string;
-        purpose: "work" | "friends" | "other";
-        invitees: string[];
-        channels: { category: string; name: string; }[];
-        user: {
+declare module "water-cooler-shared" {
+    export namespace Payload {
+        export interface CreateAccountFromInvitationPayload {
+            invitationCode: string;
             name: string;
             email: string;
             password: string;
         }
-    }
-}
 
-declare module "Responses" {
-    export interface CreateAccountFromInvitationResponse {
-        ok: boolean;
-        error?: string;
+        export interface CreateOrganizationPayload {
+            name: string;
+            domain: string;
+            purpose: "work" | "friends" | "other";
+            invitees: string[];
+            channels: { category: string; name: string; }[];
+            user: {
+                name: string;
+                email: string;
+                password: string;
+            }
+        }
     }
 
-    export interface CreateOrganizationResponse {
-        error?: string;
-        token?: string;
-    }
-
-    export interface InvitationCheckResponse {
-        valid: boolean;
+    export namespace Response {
+        export interface CreateAccountFromInvitationResponse {
+            ok: boolean;
+            error?: string;
+        }
+    
+        export interface CreateOrganizationResponse {
+            error?: string;
+            token?: string;
+        }
+    
+        export interface InvitationCheckResponse {
+            valid: boolean;
+        }
     }
 }
