3245484dc56af9c974a6666836084088f9928243
Cameron Cooper <cameron@cameron.pw>
2020-05-04 21:15:30 -0400

    ..

---
+++ index.ts
@@ -0,0 +1,38 @@
+export interface CreateAccountFromInvitationPayload {
+    invitationCode: string;
+    name: string;
+    email: string;
+    password: string;
+}
+
+export interface CreateOrganizationPayload {
+    name: string;
+    domain: string;
+    purpose: "work" | "friends" | "other";
+    invitees: string[];
+    channels: { category: string; name: string; }[];
+    user: {
+        name: string;
+        email: string;
+        password: string;
+    }
+}
+
+export interface InvitationCheckPayload {
+    code: string;
+}
+
+
+export interface CreateAccountFromInvitationResponse {
+    ok: boolean;
+    error?: string;
+}
+
+export interface CreateOrganizationResponse {
+    error?: string;
+    token?: string;
+}
+
+export interface InvitationCheckResponse {
+    valid: boolean;
+}
