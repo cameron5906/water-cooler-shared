2bd9bbb26805d0af3befde17a5396a0930bbe42f
Cameron Cooper <cameron@cameron.pw>
2020-05-04 21:11:28 -0400

    ..

--- index.d.ts
+++ index.d.ts
@@ -1,40 +1,38 @@
-declare module 'shared-types' {
-    interface CreateAccountFromInvitationPayload {
-        invitationCode: string;
+export interface CreateAccountFromInvitationPayload {
+    invitationCode: string;
+    name: string;
+    email: string;
+    password: string;
+}
+
+export interface CreateOrganizationPayload {
+    name: string;
+    domain: string;
+    purpose: "work" | "friends" | "other";
+    invitees: string[];
+    channels: { category: string; name: string; }[];
+    user: {
         name: string;
         email: string;
         password: string;
     }
-    
-    interface CreateOrganizationPayload {
-        name: string;
-        domain: string;
-        purpose: "work" | "friends" | "other";
-        invitees: string[];
-        channels: { category: string; name: string; }[];
-        user: {
-            name: string;
-            email: string;
-            password: string;
-        }
-    }
-    
-    interface InvitationCheckPayload {
-        code: string;
-    }
+}
 
-    
-    interface CreateAccountFromInvitationResponse {
-        ok: boolean;
-        error?: string;
-    }
-    
-    interface CreateOrganizationResponse {
-        error?: string;
-        token?: string;
-    }
-    
-    interface InvitationCheckResponse {
-        valid: boolean;
-    }
+export interface InvitationCheckPayload {
+    code: string;
+}
+
+
+export interface CreateAccountFromInvitationResponse {
+    ok: boolean;
+    error?: string;
+}
+
+export interface CreateOrganizationResponse {
+    error?: string;
+    token?: string;
+}
+
+export interface InvitationCheckResponse {
+    valid: boolean;
 }
