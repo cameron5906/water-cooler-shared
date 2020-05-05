fa64362d6684c4fdfca3ee9786b3802341898598
Cameron Cooper <cameron@cameron.pw>
2020-05-04 21:09:43 -0400

    ..

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,40 @@
-declare module "water-cooler-shared" {
-    export namespace Payload {
-        interface CreateAccountFromInvitationPayload {
-            invitationCode: string;
+declare module 'shared-types' {
+    interface CreateAccountFromInvitationPayload {
+        invitationCode: string;
+        name: string;
+        email: string;
+        password: string;
+    }
+    
+    interface CreateOrganizationPayload {
+        name: string;
+        domain: string;
+        purpose: "work" | "friends" | "other";
+        invitees: string[];
+        channels: { category: string; name: string; }[];
+        user: {
             name: string;
             email: string;
             password: string;
         }
-
-        interface CreateOrganizationPayload {
-            name: string;
-            domain: string;
-            purpose: "work" | "friends" | "other";
-            invitees: string[];
-            channels: { category: string; name: string; }[];
-            user: {
-                name: string;
-                email: string;
-                password: string;
-            }
-        }
+    }
+    
+    interface InvitationCheckPayload {
+        code: string;
     }
 
-    export namespace Response {
-        interface CreateAccountFromInvitationResponse {
-            ok: boolean;
-            error?: string;
-        }
     
-        interface CreateOrganizationResponse {
-            error?: string;
-            token?: string;
-        }
+    interface CreateAccountFromInvitationResponse {
+        ok: boolean;
+        error?: string;
+    }
     
-        interface InvitationCheckResponse {
-            valid: boolean;
-        }
+    interface CreateOrganizationResponse {
+        error?: string;
+        token?: string;
+    }
+    
+    interface InvitationCheckResponse {
+        valid: boolean;
     }
 }
